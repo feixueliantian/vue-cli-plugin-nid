0987786bd4af2bc92f2e9a5945059a9d68dba39d
于曼丽 <yumanli@126.com>
2022-07-04 14:55:31 +0800

    定义获取 Store 数据类型方法（getStoreStateName）

--- generator/store/store.service.js
+++ generator/store/store.service.js
@@ -1,3 +1,4 @@
+const { camelCase, upperFirst } = require('lodash');
 const path = require('path');
 
 /**
@@ -7,6 +8,15 @@ const getStoreTemplatePath = () => {
   return path.join('.', 'templates', 'store.ejs');
 };
 
+/**
+ * 获取 Store 数据类型名
+ */
+const getStoreStateName = (options) => {
+  const { store: storeName } = options;
+  return upperFirst(camelCase(storeName)) + 'StoreState';
+};
+
 module.exports = {
   getStoreTemplatePath,
+  getStoreStateName,
 };
