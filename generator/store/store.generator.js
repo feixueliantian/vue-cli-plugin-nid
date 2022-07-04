bb1221746c69420040e5a755e9d693324bc005d9
于曼丽 <yumanli@126.com>
2022-07-04 14:51:53 +0800

    准备 Store 模块模板文件

--- generator/store/store.generator.js
+++ generator/store/store.generator.js
@@ -1,6 +1,9 @@
+const { getStoreTemplatePath } = require('./store.service');
+
 const storeGenerator = (api, options) => {
   if (!options.store) return;
-  console.log('store generator ~~~');
+
+  const getStoreTemplatePath = getStoreTemplatePath();
 };
 
 module.exports = storeGenerator;
