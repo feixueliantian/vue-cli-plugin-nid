dabd387fdb762c8d9c830b52e2cb93e9e3be627e
于曼丽 <yumanli@126.com>
2022-07-04 19:53:40 +0800

    生成组件时支持使用 Vuex 的帮手方法

--- generator/component/component.generator.js
+++ generator/component/component.generator.js
@@ -6,6 +6,7 @@ const {
   getTemplatePath,
   getComponentName,
   getComponentImportStatement,
+  getComponentOptions,
 } = require('./component.service');
 
 const componentGenerator = (api, options) => {
@@ -23,6 +24,9 @@ const componentGenerator = (api, options) => {
   // 组件名
   const { componentName, componentNamePascalCase } = getComponentName(options);
 
+  // 准备选项
+  options = getComponentOptions(options);
+
   // 生成组件文件和组件样式文件
   api.render(
     {
@@ -32,6 +36,7 @@ const componentGenerator = (api, options) => {
     {
       componentName,
       componentNamePascalCase,
+      ...options,
     },
   );
 
